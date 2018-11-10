ea4106f1f4cbde01e5ab77fa9f079400d2f2e8d8
Ankit Mahlawat <34163810+AnkitMahlawat@users.noreply.github.com>
2018-11-11 00:01:00 +0530

    working code with minute error
    with minute error

--- Assignment5/Solution.cpp
+++ Assignment5/Solution.cpp
@@ -1,8 +1,25 @@
+/*
+
+In this program i remeorized (which i kind of forgotten by now) a conecept that memory allocated on stack frees when the function acquiring that stack memory goes out of scope  
+os allocates memory on stack when we donot use new operator during allocation of memory.
+In this program my heap class requires a array based memory whose size will vary so I had to assign memory on runtime so in constructor of heap class I didnot use new operator
+instead i wrote line no 128 and some other places in class heap as flight temp[CAPACITY];(rest of the code changed accoring to this line but this line was the main culprit) which allocated memory on stack and when
+constuctor function ended it all vanished and the list in heap class was pointing to a freed memory location hence SIGSEV error came.now I changed it to list = new flight[CAPACITY]
+which allocated memory on heap and everything is fine now.
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
+
+int new_arrTime(int arrTime){
+    if(arrTime%100<30){
+        return arrTime+30;
+    }else return arrTime+70;
+}
+
 class flight{
-	int Atime,Dtime,price,to,from;
+	int Atime=-0100,Dtime,price,to,from;
 	string plane;
 public:
 	flight(){}
@@ -28,10 +45,143 @@ public:
 	bool compare(flight a){
 			return this->Dtime>a.Dtime;
 	}
+	
+	int get_Deptime(){
+	    return Dtime;
+	}
+	
+	int get_Arrtime(){
+	    return Atime;
+	}
+	
+	int get_from(){
+	    return from;
+	}
+	
+	int get_to(){
+	    return to;
+	}
+	
+	int get_price(){
+	    return price;
+	}
 
 	void printFlight(){
-		cout<<from<<" "<<to<<" "<<Dtime<<" "<<Atime<<" "<<plane<<" "<<price<<endl;
+		cout<<this->from<<" "<<this->to<<" "<<this->Dtime<<" "<<this->Atime<<" "<<this->plane<<" "<<this->price<<endl;
 	}
+	
+
+};
+
+class node{
+    flight f;
+    int Tprice=0;
+    bool valid=false;
+    node* link=nullptr;
+public:
+    node(){}
+   
+    node(flight fl,int Tpr,node* l,bool v){
+        Tprice=Tpr;
+        f=fl;
+        link=l;
+        valid=v;
+    }
+    
+    flight get_flight(){
+        return f;
+    }
+    
+    bool get_valid(){
+        return valid;
+    }
+    
+    node* get_link(){
+        return link;
+    }
+    
+    int get_Tprice(){
+        return Tprice;
+    }
+    
+    void set_valid(bool v){
+        valid=v;
+    }
+    
+    void set_ptr(node* ptr){
+        link=ptr;
+    }
+};
+
+class a_list{
+    node* a;
+    int size;
+    int* size_n;
+    //node a[n];
+public:
+    a_list(int size){
+        this->size=size;
+        a=new node[size];
+        size_n=new int[size];
+        for (int i = 0; i < size; i++) {
+            size_n[i]=0;
+        }
+    }
+    
+    node get_i(int i){
+        return a[i];
+    }
+    
+    bool check(int index,int Dep_time,int to_i,flight current,int *ptr ){
+        node check_node=a[index];
+        node* nodeptr=check_node.get_link();
+        while(true){
+            if(new_arrTime(check_node.get_flight().get_Arrtime())<=Dep_time){
+                if(!a[to_i].get_valid()||(current.get_Arrtime()<a[to_i].get_flight().get_Arrtime() || current.get_price()+check_node.get_Tprice()<a[to_i].get_Tprice())){
+                    *ptr=current.get_price()+check_node.get_Tprice();
+                    return true;
+                }
+            }
+            if(nodeptr==nullptr){
+                return false;
+            }else{
+                check_node=*nodeptr;
+                nodeptr=check_node.get_link();
+            }
+        }
+    }
+    
+    void add(int index,node n){
+        node add_node=a[index];
+        if(!add_node.get_valid()){
+            a[index]=n;
+            return;
+        }
+        if(add_node.get_Tprice()>=n.get_Tprice()){
+            a[index]=n;
+            n.set_ptr(&add_node);
+            return;
+        }
+        
+        node* ahead=add_node.get_link();
+        node* follow=&add_node;
+        while(follow!=nullptr){
+            if(ahead==nullptr){
+                follow->set_ptr(&n);
+                return;
+            }
+            if(ahead->get_Tprice()>n.get_Tprice()){
+                n.set_ptr(ahead);
+                follow->set_ptr(&n);
+            }
+            follow=ahead;
+            ahead=ahead->get_link();
+        }
+                    
+
+        return;
+    }
+
 };
 
 
@@ -105,8 +255,7 @@ class heap{
 public:
 	heap(int capacity){
 		CAPACITY=capacity;
-		flight temp[CAPACITY];
-		list=temp;
+		list = new flight[CAPACITY];
 		size=0;
 	}
 
@@ -114,13 +263,12 @@ public:
 		CAPACITY=a.CAPACITY;
 		size=a.size;
         
-		flight temp[CAPACITY];
+		list =new flight[CAPACITY];
 		for (int i = 0; i < size; ++i){
-			flight f(a.list[i]);
-			a.list[i].printFlight();
-			temp[i]=f;
+			
+			list[i]=a.list[i];
 		}
-		list=temp;
+		
 	}
 
 	bool isEmpty(){
@@ -129,12 +277,11 @@ public:
 
 	void resize(){
 		CAPACITY=2*CAPACITY;
-		flight temp[CAPACITY];
+		flight *temp = new flight[CAPACITY];
 		for (int i = 0; i < size; ++i){
 			temp[i]=list[i];
 		}
 		list=temp;
-		free(temp);
 	}
 
 	void insert(flight l[],int size,int capacity){
@@ -163,6 +310,10 @@ public:
 		heapifyD();
 		return temp;
 	}
+	
+	flight getMin(){
+	    return list[0];
+	}
 
 	void printHeap(){
 		for (int i = 0; i < size; ++i){
@@ -173,21 +324,46 @@ public:
 	
 };
 
-int main(int argc, char const *argv[]){
-	if (argc!=2){
-		printf("usage:%s filename\n",argv[0] );
-		exit(0);
+void fun_tripPlanner(int start_city,int end_city,int Dep_time,int Arr_time,heap temp,int Ncities,int Nplanes){
+    a_list arr_list(Ncities+1);
+	flight fl;
+	node src(fl,0,nullptr,true);
+	arr_list.add(start_city,src);
+	while(temp.getMin().get_Deptime()<Arr_time&&!temp.isEmpty()){
+	    flight current=temp.deleteMin();
+	    if(current.get_Deptime()>Dep_time&&current.get_Arrtime()<Arr_time){
+            node from_node=arr_list.get_i(current.get_from());
+            if(from_node.get_valid()){
+                int ptr_price;
+                if(arr_list.check(current.get_from(),current.get_Deptime(),current.get_to(),current,&ptr_price)){
+                    node to_add(current,ptr_price,nullptr,true);
+                    arr_list.add(current.get_to(),to_add);
+                }
+            }
+	    }
 	}
+	
+	if(arr_list.get_i(end_city).get_valid()){
+	    cout<<arr_list.get_i(end_city).get_Tprice()<<endl;
+	}else cout<<-1<<endl;
 
-	#ifndef ONLINE_JUDGE
-    // for getting input from input.txt
-    freopen(argv[1], "r", stdin);
-    // for writing output to output.txt
-    freopen("output.txt", "w", stdout);
-	#endif
+}
+
+int main(int argc, char const *argv[]){
+// 	if (argc!=2){
+// 		printf("usage:%s filename\n",argv[0] );
+// 		exit(0);
+// 	}
+
+// 	#ifndef ONLINE_JUDGE
+//     // for getting input from input.txt
+//     freopen(argv[1], "r", stdin);
+//     // for writing output to output.txt
+//     freopen("output.txt", "w", stdout);
+// 	#endif
 
     int Ncities,Nplanes;
-    scanf("%d %d",&Ncities,&Nplanes);
+    cin>>Ncities>>Nplanes;
     
     flight list[Nplanes]; 
     heap H(Nplanes);
@@ -208,11 +384,9 @@ int main(int argc, char const *argv[]){
     for (int i = 0; i < Queries; ++i){
 
     	heap temp(H);
-    	temp.printHeap();
     	int start_city,end_city,Dep_time,Arr_time;
     	cin>>start_city>>end_city>>Dep_time>>Arr_time;
-
+    	fun_tripPlanner(start_city,end_city,Dep_time,Arr_time,temp,Ncities,Nplanes);
     }
-
 	return 0;
 }
